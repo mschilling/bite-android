9e407b403f0895acbce47fd0f035bde13b0d45db
cassshh <casvd@hotmail.com>
2016-09-29 14:26:02 +0200

    Splash screen stuff

--- app/src/main/java/com/move4mobile/apps/bite/LauncherActivity.java
+++ app/src/main/java/com/move4mobile/apps/bite/LauncherActivity.java
@@ -3,9 +3,6 @@ package com.move4mobile.apps.bite;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.view.Window;
-import android.view.WindowManager;
 
 public class LauncherActivity extends Activity {
 
@@ -13,16 +10,6 @@ public class LauncherActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        int theme = PreferenceManager.getDefaultSharedPreferences(this).getInt("pref_theme", R.style.AppTheme_Light);
-        if(theme == R.style.AppTheme_Light){
-            setTheme(R.style.AppTheme_Launcher_Light);
-        } else {
-            setTheme(R.style.AppTheme_Launcher);
-        }
-
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
