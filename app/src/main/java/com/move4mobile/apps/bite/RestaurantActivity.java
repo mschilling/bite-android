027f8bd3d81d4c51a0a15a33413f08726b6e0388
cassshh <casvd@hotmail.com>
2016-10-03 16:49:43 +0200

    Restaurant screen concept.

--- app/src/main/java/com/move4mobile/apps/bite/RestaurantActivity.java
+++ app/src/main/java/com/move4mobile/apps/bite/RestaurantActivity.java
@@ -3,7 +3,19 @@ package com.move4mobile.apps.bite;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.ListViewCompat;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class RestaurantActivity extends AppCompatActivity {
 
@@ -22,5 +34,44 @@ public class RestaurantActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        RecyclerView rv = (RecyclerView) findViewById(R.id.recycler_view);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        rv.setNestedScrollingEnabled(false);
+
+        final ArrayList<String> mItems = new ArrayList<>();
+
+        RecyclerView.Adapter adapter = new RecyclerView.Adapter() {
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_view_menu_item, parent, false);
+                return new CViewHolder(view);
+            }
+
+            @Override
+            public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+            }
+
+            @Override
+            public int getItemCount() {
+                return mItems.size();
+            }
+        };
+
+        rv.setAdapter(adapter);
+
+        for(int i = 0; i < 20; i++){
+            mItems.add(""+i);
+            adapter.notifyItemInserted(mItems.size() - 1);
+        }
+
     }
 }
+
+class CViewHolder extends RecyclerView.ViewHolder {
+
+    public CViewHolder(View itemView) {
+        super(itemView);
+    }
+}
+
