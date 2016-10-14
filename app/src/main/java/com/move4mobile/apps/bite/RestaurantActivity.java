e3157acab07d91adf5e3a342959aaf4efa36253f
cassshh <casvd@hotmail.com>
2016-10-14 16:49:53 +0200

    Added Glide. Bite restaurant Card No.1 Besteller. Minor Changes

--- app/src/main/java/com/move4mobile/apps/bite/RestaurantActivity.java
+++ app/src/main/java/com/move4mobile/apps/bite/RestaurantActivity.java
@@ -1,6 +1,10 @@
 package com.move4mobile.apps.bite;
 
+import android.graphics.Bitmap;
 import android.preference.PreferenceManager;
+import android.provider.ContactsContract;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,6 +13,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
 
 import java.util.ArrayList;
 
@@ -65,6 +73,19 @@ public class RestaurantActivity extends AppCompatActivity {
             mItems.add(""+i);
             adapter.notifyItemInserted(mItems.size() - 1);
         }
+
+        final ImageView imageView = (ImageView) findViewById(R.id.image_besteller);
+        Glide.with(this).load("https://avatars2.githubusercontent.com/u/7045335?v=3&s=400").asBitmap().centerCrop().into(new BitmapImageViewTarget(imageView) {
+            @Override
+            protected void setResource(Bitmap resource) {
+                RoundedBitmapDrawable circularBitmapDrawable =
+                        RoundedBitmapDrawableFactory.create(getResources(), resource);
+                circularBitmapDrawable.setCircular(true);
+                imageView.setImageDrawable(circularBitmapDrawable);
+            }
+        });
+
+
     }
 }
 
