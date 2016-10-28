1d846027534b8bad10e74a53cb9847b21819b9cb
cassshh <casvd@hotmail.com>
2016-10-28 14:48:57 +0200

    Login screen. Update all dependencies

---
+++ app/src/main/java/com/move4mobile/apps/bite/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.move4mobile.apps.bite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.common.SignInButton;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
