b02ca4a1fa0b9838742ccaef8141f47f0974dacf
cassshh <casvd@hotmail.com>
2016-10-28 10:39:37 +0200

    Custom stats title element

---
+++ app/src/main/java/com/move4mobile/apps/bite/RelativeLayoutStatsTitle.java
@@ -0,0 +1,40 @@
+package com.move4mobile.apps.bite;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.GradientDrawable;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by casvd on 27-10-2016.
+ */
+
+public class RelativeLayoutStatsTitle extends RelativeLayout {
+    public RelativeLayoutStatsTitle(Context context) {
+        super(context);
+    }
+    public RelativeLayoutStatsTitle(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setCircleColor(context, attrs);
+    }
+
+    public RelativeLayoutStatsTitle(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setCircleColor(context, attrs);
+    }
+
+    private void setCircleColor(Context ctx, AttributeSet attrs) {
+        TypedArray a = ctx.obtainStyledAttributes(attrs, R.styleable.RelativeLayoutStatsTitle);
+        int color = a.getColor(0, getResources().getColor(R.color.colorAccent, ctx.getTheme()));
+        setStatsTitleColor(ctx, color);
+        a.recycle();
+    }
+
+    public void setStatsTitleColor(Context ctx, int color){
+        GradientDrawable bgDrawable = (GradientDrawable) getBackground();
+        bgDrawable.setColor(color);
+    }
+}
