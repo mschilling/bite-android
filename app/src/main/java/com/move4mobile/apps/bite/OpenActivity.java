a9b799c21fd98821cd0e262122bc5f86a83ef9b0
cassshh <casvd@hotmail.com>
2017-01-25 16:14:06 +0100

    Add time selector. Adding Bites in next update

--- app/src/main/java/com/move4mobile/apps/bite/OpenActivity.java
+++ app/src/main/java/com/move4mobile/apps/bite/OpenActivity.java
@@ -5,14 +5,16 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.move4mobile.apps.bite.customlayoutclasses.TextViewCustom;
 import com.move4mobile.apps.bite.objects.Store;
 
 public class OpenActivity extends AppCompatActivityFireAuth {
@@ -20,8 +22,10 @@ public class OpenActivity extends AppCompatActivityFireAuth {
     private static final String TAG = "OpenActivity";
 
     private String key;
+    private int time;
 
     private RelativeLayout mOpen;
+    private SeekBar mOpenTime;
     private RecyclerView mRecyclerViewStores;
 
     private FirebaseDatabase mDatabase;
@@ -39,42 +43,40 @@ public class OpenActivity extends AppCompatActivityFireAuth {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         mOpen = (RelativeLayout) findViewById(R.id.bite_open_button);
+        mOpenTime = (SeekBar) findViewById(R.id.bite_open_time);
         mRecyclerViewStores = (RecyclerView) findViewById(R.id.recycler_view_stores);
         mRecyclerViewStores.setHasFixedSize(false);
         mRecyclerViewStores.setNestedScrollingEnabled(false);
         mRecyclerViewStores.setLayoutManager(new LinearLayoutManager(this));
 
+        mOpenTime.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                updateTime(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                updateTime(seekBar.getProgress());
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                updateTime(seekBar.getProgress());
+            }
+        });
+        mOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(key != null && !key.isEmpty() && time > 0) {
+                    Toast.makeText(OpenActivity.this, "Gonna create Bite in next update. Stay posted bros", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        });
+
         //Firebase Database
         mDatabase = FirebaseDatabase.getInstance();
-    }
-
-    @Override
-    protected void onLoggedIn() {
-        super.onLoggedIn();
-        mRefStores = mDatabase.getReference("stores");
-        if(adapter == null) {
-            adapter = new StoreAdapter(Store.class, R.layout.bite_card_open, StoreViewHolder.class, mRefStores, OpenActivity.this);
-            adapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
-                @Override
-                public void onItemRangeRemoved(int positionStart, int itemCount) {
-                    Log.e(TAG, "Removed at " + positionStart + "|" + itemCount);
-                    View v = mRecyclerViewStores.getChildAt(positionStart);
-                    Log.e(TAG, "Is it checked: " + ((RadioButton)v.findViewById(R.id.bite_open_radio)).isChecked());
-                    notifySelectStore("", -1);
-                    super.onItemRangeRemoved(positionStart, itemCount);
-                    adapter.notifyDataSetChanged();
-                }
-            });
-            mRecyclerViewStores.setAdapter(adapter);
-        }
-    }
-
-    @Override
-    protected void onLoggedOut() {
-        super.onLoggedOut();
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-        finish();
     }
 
     protected boolean notifySelectStore(String key, int position) {
@@ -90,6 +92,40 @@ public class OpenActivity extends AppCompatActivityFireAuth {
     }
 
     private void updateButton() {
-        mOpen.setBackground(getDrawable(key == null || key.isEmpty() ? R.drawable.bring_it_button_locked : R.drawable.bring_it_button));
+        mOpen.setBackground(getDrawable(key == null || key.isEmpty() || time<=0 ? R.drawable.bring_it_button_locked : R.drawable.bring_it_button));
+    }
+
+    private void updateTime(int min) {
+        TextViewCustom text = (TextViewCustom) findViewById(R.id.bite_open_time_text);
+        text.setText(min * 10 + " minuten");
+        this.time = min;
+        updateButton();
+    }
+
+    @Override
+    protected void onLoggedIn() {
+        super.onLoggedIn();
+        mRefStores = mDatabase.getReference("stores");
+        if(adapter == null) {
+            adapter = new StoreAdapter(Store.class, R.layout.bite_card_open, StoreViewHolder.class, mRefStores, OpenActivity.this);
+            adapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+                @Override
+                public void onItemRangeRemoved(int positionStart, int itemCount) {
+                    View v = mRecyclerViewStores.getChildAt(positionStart);
+                    notifySelectStore("", -1);
+                    super.onItemRangeRemoved(positionStart, itemCount);
+                    adapter.notifyDataSetChanged();
+                }
+            });
+            mRecyclerViewStores.setAdapter(adapter);
+        }
+    }
+
+    @Override
+    protected void onLoggedOut() {
+        super.onLoggedOut();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
